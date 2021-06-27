8f3a13f507544e3b649aad622b1eb7c05be9f4b9
中山諒 <>
2021-06-27 16:30:53 +0900

    タスクの追加

--- TODO/js/main.js
+++ TODO/js/main.js
@@ -1,3 +1,20 @@
 var app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        newItem: '',
+        todos: []
+    },
+    methods: {
+        addItem(event){
+            // alert();
+            // newItemが空だったらここで処理終了
+            if (this.newItem == '') return;
+            var todo = {
+                item: this.newItem
+            };
+
+            this.todos.push(todo);
+            this.newItem = '';
+        }
+    }
 })
